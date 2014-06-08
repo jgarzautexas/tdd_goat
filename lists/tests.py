9e6112b71758f0f52ebfe9df9844310857b4316f
Jonathan <jonathan@jonathan-VirtualBox.(none)>
2014-06-07 19:44:34 -0500

    first unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,24 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
+class HomePageTest(TestCase):
+    def test_root_url(self):
+        found = resolve('/')
+	self.assertEqual(found.func, home_page)
+#class SimpleTest(TestCase):
+#    def test_basic_addition(self):
+#        """
+#	Tests that 1 + 1 always equals 2.
+#	"""
+#	self.assertEqual(1+1, 2)
+#
+#    def test_true(self):
+#        """
+#	Tests True
+#	"""
+#        self.assertTrue(True)
+#
+
+
